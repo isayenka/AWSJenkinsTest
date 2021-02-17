a4584c006a97cf367fb2a4a9c3a16e5fe393f6a7
isayenka <75360697+isayenka@users.noreply.github.com>
2021-02-17 23:44:45 +0300

    added path

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 
 @fixture(scope='function')
 def chrome_browser():
-    chrome_options = webdriver.ChromeOptions()
+    chrome_options = webdriver.ChromeOptions("/usr/local/bin")
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--headless')
