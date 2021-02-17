fa13a65b2c482207df8f66f3a7cfead688c9f838
isayenka <robertoopython@gmail.com>
2021-02-17 11:37:35 +0300

    added headless option

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ def chrome_browser():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
     browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.maximize_window()
     return browser
