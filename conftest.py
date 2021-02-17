8386f72e667ee45e3b7f9e7476e9a7845cf20326
isayenka <robertoopython@gmail.com>
2021-02-17 11:12:38 +0300

    added options

--- conftest.py
+++ conftest.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 
 @fixture(scope='function')
 def chrome_browser():
-    browser = webdriver.Chrome()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    browser = webdriver.Chrome(chrome_options=chrome_options)
     browser.maximize_window()
     return browser
