384c65d9ef8acfac3fde656919fd10db00edbc4b
isayenka <75360697+isayenka@users.noreply.github.com>
2021-02-18 00:07:58 +0300

    2

--- conftest.py
+++ conftest.py
@@ -8,6 +8,6 @@ def chrome_browser():
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--headless')
-    browser = webdriver.Chrome(chrome_options=chrome_options)
+    browser = webdriver.Chrome(chrome_options=chrome_options, executable_path=r"chromedriver",)
     browser.maximize_window()
     return browser
